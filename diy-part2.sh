4c366b05eef0262ea9714d8ad64024e8b3fc246b
lieet <59359907+flrz@users.noreply.github.com>
2022-10-09 21:13:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,5 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-sed -i 's/192.168.1.1/192.168.105.252/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/21.21.21.252/g' package/base-files/files/bin/config_generate
 rm openwrt/package/lean/luci-theme-argon -rf
