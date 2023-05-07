96ad7d1bb32f27f7c273b7d43b89098275cc472f
lieet <59359907+flrz@users.noreply.github.com>
2023-05-07 13:25:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,7 +20,8 @@
 #echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
 #echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git lua https://github.com/jerrykuku/lua-maxminddb' >>feeds.conf.default
+echo 'src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
+#echo 'src-git lua https://github.com/jerrykuku/lua-maxminddb' >>feeds.conf.default
 #echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon' >>feeds.conf.default
 #echo 'src-git vssr https://github.com/jerrykuku/luci-app-vssr' >>feeds.conf.default
 #echo 'src-git vssr https://github.com/jerrykuku/lua-maxminddb' >>feeds.conf.default
