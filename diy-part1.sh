190b23609d47328201e08749bf86f5d69c50a55a
lieet <59359907+flrz@users.noreply.github.com>
2023-05-08 13:24:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,8 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
@@ -20,7 +22,7 @@
 #echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
 #echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
+#echo 'src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 #echo 'src-git lua https://github.com/jerrykuku/lua-maxminddb' >>feeds.conf.default
 #echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon' >>feeds.conf.default
 #echo 'src-git vssr https://github.com/jerrykuku/luci-app-vssr' >>feeds.conf.default
