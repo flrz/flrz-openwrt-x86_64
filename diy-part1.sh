117b5c61175a19c463e3ca1ad92ddc016e4b553f
lieet <59359907+flrz@users.noreply.github.com>
2023-08-27 08:10:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,12 +15,13 @@
 #sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a src-git flrz https://github.com/flrz/flrz-package' feeds.conf.default
+#sed -i '$a src-git flrz https://github.com/flrz/flrz-package' feeds.conf.default
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
-#echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #echo 'src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 #echo 'src-git lua https://github.com/jerrykuku/lua-maxminddb' >>feeds.conf.default
