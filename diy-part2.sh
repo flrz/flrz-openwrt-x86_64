f602509e536091d182613102c59a9e94b5d6209d
lieet <59359907+flrz@users.noreply.github.com>
2023-09-21 10:54:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.102.252/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.102.253/g' package/base-files/files/bin/config_generate
 
 #2. Replace with JerryKuKu’s Argon
 #rm -rf luci-theme-argone
