3136b08831d8d062e6fc7317b268e9aea4de2c39
lieet <59359907+flrz@users.noreply.github.com>
2024-02-15 18:16:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,11 +14,11 @@
 sed -i 's/192.168.1.1/192.168.7.1/g' package/base-files/files/bin/config_generate
 
 #2. Replace with JerryKuKu’s Argon
-#cd lede/package/lean
+cd lede/packages/lean
 #rm -rf luci-theme-argon
-#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
-cd openwrt/packages
-git clone https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
+#cd openwrt/packages
+#git clone https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 
 #cd lede/package/lean
