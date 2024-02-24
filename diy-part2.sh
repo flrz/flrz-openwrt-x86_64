17eddde858f7eedea26131bcef757c7f1b3a1d21
lieet <59359907+flrz@users.noreply.github.com>
2024-02-24 16:46:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,6 +21,9 @@ sed -i 's/192.168.1.1/192.168.7.1/g' package/base-files/files/bin/config_generat
 #git clone https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argone/' feeds/luci/collections/luci/Makefile
 
+#3. Clear the login password
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+
 #cd lede/package/lean
 #rm -rf luci-app-argon-config # if have
 #git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git luci-app-argon-config
