27536ca8f94cc703bf228536ec8882e1c4a2b919
lieet <59359907+flrz@users.noreply.github.com>
2024-10-18 20:46:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,20 +11,10 @@
 #
 
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.217.251/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-#2. Replace with JerryKuKu’s Argon
-#cd lede/package/lean
-#rm -rf luci-theme-argon
-#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
-#cd openwrt/packages
-#git clone https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
-#sed -i 's/luci-theme-bootstrap/luci-theme-argone/' feeds/luci/collections/luci/Makefile
-#sed -i 's/luci-theme-bootstrap/luci-theme-argone/' feeds/luci/collections/luci/Makefile
+# Modify default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-#3. Clear the login password
-#sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
-
-#cd lede/package/lean
-#rm -rf luci-app-argon-config # if have
-#git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git luci-app-argon-config
+# Modify hostname
+#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
