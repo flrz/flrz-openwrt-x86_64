18a7724a3136d8cbb2886385ca1e297df83a19e0
lieet <59359907+flrz@users.noreply.github.com>
2024-11-02 22:15:00 +0800

    Update diy-part22.sh

--- diy-part22.sh
+++ diy-part22.sh
@@ -1,2 +1,3 @@
-
+#1. Modify default IP
+sed -i 's/192.168.1.1/192.168.7.1/g' package/base-files/files/bin/config_generate
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
