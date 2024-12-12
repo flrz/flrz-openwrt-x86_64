9905d0999e72d827083998976f2ad8c43a7b58b0
lieet <59359907+flrz@users.noreply.github.com>
2024-12-12 10:28:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,13 +13,13 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
-#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+sed -i '1i src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-sed -i '1i src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
-sed -i '2i src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+#sed -i '1i src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
+#sed -i '2i src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 #echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
-sed -i '3i src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
+#sed -i '3i src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
 #echo 'src-git themes https://github.com/flrz/lede-themes' >>feeds.conf.default
 #echo 'src-git adguardhome https://github.com/flrz/adguardhome' >>feeds.conf.default
-sed -i '4i src-git argon https://github.com/jerrykuku/luci-theme-argon.git' feeds.conf.default
+#sed -i '4i src-git argon https://github.com/jerrykuku/luci-theme-argon.git' feeds.conf.default
