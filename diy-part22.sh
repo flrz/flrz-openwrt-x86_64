6afdcc66a4d45368bcee2dff8929390ff184233f
lieet <59359907+flrz@users.noreply.github.com>
2024-12-12 10:31:35 +0800

    Update diy-part22.sh

--- diy-part22.sh
+++ diy-part22.sh
@@ -2,4 +2,4 @@
 sed -i 's/192.168.1.1/192.168.7.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
